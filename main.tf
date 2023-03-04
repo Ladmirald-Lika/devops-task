fe596287561f38d41e09adac10814f359e7ed3fa
Ladmirald-Lika <126253540+Ladmirald-Lika@users.noreply.github.com>
2023-03-04 18:11:11 +0100

    Add files that were tested locally

---
+++ main.tf
@@ -0,0 +1,74 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+// security group creation
+resource "aws_security_group" "allow-all-sg" {
+name_prefix = "allow-all-sg"
+#vpc_id = "vpc-094258820a9889486"
+ingress {
+    cidr_blocks = [
+      "0.0.0.0/0"
+    ]
+from_port = 22
+    to_port = 22
+    protocol = "tcp"
+  }
+// Terraform removes the default rule
+  egress {
+   from_port = 0
+   to_port = 0
+   protocol = "-1"
+   cidr_blocks = ["0.0.0.0/0"]
+ }
+}
+// Ec2 Creation
+resource "aws_instance" "control1" {
+  ami           = "ami-09cd747c78a9add63"
+  instance_type = "t3.micro"
+  key_name      = "app-ssh-key"
+  vpc_security_group_ids = [aws_security_group.allow-all-sg.id]
+  ebs_block_device {
+  device_name = "/dev/sda1"
+  volume_size = 20
+  }
+  tags = {
+    Name = "control1"
+  }
+}
+
+resource "aws_instance" "worker1" {
+  ami           = "ami-09cd747c78a9add63"
+  instance_type = "t3.micro"
+  key_name      = "app-ssh-key"
+  vpc_security_group_ids = [aws_security_group.allow-all-sg.id]
+  ebs_block_device {
+  device_name = "/dev/sda1"
+  volume_size = 20
+  }
+  tags = {
+    Name = "worker1"
+  }
+}
+resource "aws_instance" "worker2" {
+  ami           = "ami-09cd747c78a9add63"
+  instance_type = "t3.micro"
+  key_name      = "app-ssh-key"
+  vpc_security_group_ids = [aws_security_group.allow-all-sg.id]
+  ebs_block_device {
+  device_name = "/dev/sda1"
+  volume_size = 20
+  }
+  tags = {
+    Name = "worker2"
+  }
+}
+output "ec2_public_ip_control1" {
+  value = ["${aws_instance.control1.public_ip}"]
+}
+output "ec2_public_ip_worker1" {
+  value = ["${aws_instance.worker1.public_ip}"]
+}
+output "ec2_public_ip_control1" {
+  value = ["${aws_instance.worker1.public_ip}"]
+}
